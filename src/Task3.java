032ec60cf39bee468a89c07a6db7150b32b20438
mchernyshou <maxchernyshou@gmail.com>
2019-08-28 17:41:00 +0300

    Delete comments for Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -10,20 +10,6 @@ public class Task3 {
         int d = (h - hour) / 24;
         int days = d % 24;
         int w = (d - days) / 7;
-        int week = w % 7;
-
-
-
-//        System.out.println("s = " + s);
-//        System.out.println("sec = " + sec);
-//        System.out.println("m = " + m);
-//        System.out.println("min = " + min);
-//        System.out.println("h = " + h);
-//        System.out.println("hour = " + hour);
-//        System.out.println("d = " + d);
-//        System.out.println("days = " + days);
-//        System.out.println("w = " + w);
-//        System.out.println("week = " + week);
 
 
         System.out.println("A) " + m + " минут " + sec + " секунд");
