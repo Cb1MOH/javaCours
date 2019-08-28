583d728267c078f9bb8ce620c134836c51fb76df
mchernyshou <maxchernyshou@gmail.com>
2019-08-28 18:11:28 +0300

    while loop for task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,7 +1,13 @@
 public class Task4 {
     public static void main(String[] args) {
-        int a = 1234;
-        int b = Integer.reverse(a);
-        System.out.println(b);
+        int number = 1234;
+//        int revertedNumber = Integer.reverse(number);
+        int revertedNumber = 0;
+        while (number != 0) {
+            revertedNumber = revertedNumber * 10 + number % 10;
+            number = number / 10;
+        }
+
+        System.out.println(revertedNumber);
     }
 }
