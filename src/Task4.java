909727c99e6d79357e7d9c59af02e076f2bbd2ae
mchernyshou <maxchernyshou@gmail.com>
2019-08-29 14:31:22 +0300

    Task 4 while cycle implementation

--- src/Task4.java
+++ src/Task4.java
@@ -1,7 +1,6 @@
 public class Task4 {
     public static void main(String[] args) {
         int number = 1234;
-//        int revertedNumber = Integer.reverse(number);
         int revertedNumber = 0;
         while (number != 0) {
             revertedNumber = revertedNumber * 10 + number % 10;
