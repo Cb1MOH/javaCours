f37e8a05b711a31e587308c2e902b5afcd093196
mchernyshou <maxchernyshou@gmail.com>
2019-08-30 10:32:14 +0300

    Lesson 2 file added

---
+++ Lesson2/src/Arithmetics.java
@@ -0,0 +1,44 @@
+public class Arithmetics {
+    public static void main(String[] args) {
+//        int a=5;
+//        int b = 23;
+//
+//        int result = b / a;
+//
+//        System.out.println(result);
+//
+//
+//        int a = 5;
+//        int b = 42;
+//        boolean result = a == b;
+//
+//        System.out.println(result);
+//
+//
+//        String hello = "Hello world";
+//        String hello2 = new String("Hello world");
+//        String hello3 = hello;
+//        String hello4 = hello2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Is hello is = Hello?" + (hello == hello2));
+
+
+    }
+}
