728e0d6d153ca319162ac0aced0ce95b32507b31
mchernyshou <maxchernyshou@gmail.com>
2019-09-04 16:21:24 +0300

    lesson 3 notes

--- Lesson3/src/Lesson3.java
+++ Lesson3/src/Lesson3.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class Lesson3 {
     public static void main(String[] args) {
 
+        Random rand = new Random();
+
+        int[][] marks = new int[4][];
+
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = new int[rand.nextInt(3) + 2];
+        }
+            for (int i = 0; i < marks.length; i++) {
+                for (int j = 0; j < marks[i].length; j++) {
+                    System.out.println(marks[i][j] + "            ");
+                }
+            }
+
+        }
     }
 }
+
