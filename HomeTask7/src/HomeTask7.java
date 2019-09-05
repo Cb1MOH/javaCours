d5b766cf8225548bf22142cb073facf4647715f0
mchernyshou <maxchernyshou@gmail.com>
2019-09-05 14:20:01 +0300

    Task 7 implementation

--- HomeTask7/src/HomeTask7.java
+++ HomeTask7/src/HomeTask7.java
@@ -1,5 +1,19 @@
+//Задание 7
+//        Имеется прямоугольное отверстие размерами a и b, определить, можно ли его
+//        полностью закрыть круглой картонкой радиусом r.
+
+
 public class HomeTask7 {
     public static void main(String[] args) {
-
+        double sideA = 3;
+        double sideB = 5;
+        double radius = 6;
+        double requiredRadius = Math.sqrt(((sideA * sideA) + (sideB * sideB)));
+        if (radius >= requiredRadius) {
+            System.out.println("Rectangle with side A with value " + sideA + " and side B with value " + sideB + " can be covered by circle with r = " + radius);
+        } else {
+            System.out.println("Rectangle with side A with value " + sideA + " and side B with value " + sideB + " can NOT be covered by circle with r = " + radius);
+        }
     }
 }
+
