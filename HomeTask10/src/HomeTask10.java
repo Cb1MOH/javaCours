ee282d1d4d80c5201a8d216538859718bac1913b
mchernyshou <maxchernyshou@gmail.com>
2019-09-06 12:12:36 +0300

    Factorial calculation logic is updated

--- HomeTask10/src/HomeTask10.java
+++ HomeTask10/src/HomeTask10.java
@@ -1,10 +1,14 @@
 //Задание 10
 //        Посчитать факториал числа в границах от 10 до 15 (не используя рекурсию).
 
+import java.util.Random;
+
 public class HomeTask10 {
     public static void main(String[] args) {
-        int factorial = 1;
-        for (int i = 10; i <=15 ; i++) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(5) + 10;
+        long factorial = 1;
+        for (int i = 2; i <=randomNumber ; i++) {
             factorial = factorial * i;
         }
         System.out.println(factorial);
