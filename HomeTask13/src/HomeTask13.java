f098f7e22685eb477b81c07c2ebc3601d686d9c2
mchernyshou <maxchernyshou@gmail.com>
2019-09-09 14:58:46 +0300

    task 13 implementation

--- HomeTask13/src/HomeTask13.java
+++ HomeTask13/src/HomeTask13.java
@@ -1,5 +1,19 @@
+//Задание 13
+//        Создать последовательность случайных чисел, найти и вывести наибольшее
+//        из них.
+
+import java.util.Arrays;
+
 public class HomeTask13 {
     public static void main(String[] args) {
+        int array[] = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            int randomInt = (int)(Math.random() * 100);
+            array[i] = randomInt;
+            System.out.println(array[i]);
+        }
 
+        Arrays.sort(array);
+        System.out.println("Last number in array is " + array[array.length -1]);
     }
 }
