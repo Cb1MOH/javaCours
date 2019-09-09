6fa4d27e944516a82fac96dcaff95f9c49b2e77e
mchernyshou <maxchernyshou@gmail.com>
2019-09-09 17:13:51 +0300

    task 15 implementation

--- HomeTask15/src/HomeTask15.java
+++ HomeTask15/src/HomeTask15.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class HomeTask15 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int random = rand.nextInt(11);
+        int array[] = new int[random];
+
+        //Generate random array and print it
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(21);
+            System.out.print(array[i] + "    ");
+        }
+        System.out.println("");
+
+
+        //Reverse the array and print it
+        for (int i = 0; i < array.length / 2; i++) {
+            int value = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = value;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "    ");
+        }
+
+
+    }
+
+
 }
+
