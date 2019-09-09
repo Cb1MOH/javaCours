d607c863a937e3ef8b982e19bd27655cd9a133cb
mchernyshou <maxchernyshou@gmail.com>
2019-09-09 16:44:39 +0300

    task 14 implementation

---
+++ HomeTask14/src/HomeTask14.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class HomeTask14 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int minimum = 999;
+        int maximum = 0;
+        int random = rand.nextInt(11);
+        int array[] = new int[random];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.print(array[i] + "   ");
+        }
+
+// Find minimum mark and index
+        System.out.println("");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minimum) {
+                minimum = array[i];
+            }
+        }
+        System.out.println("Minimum mark = " + minimum);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == minimum) {
+                System.out.println("Index of min mark = " + i);
+                break;
+            }
+        }
+
+// Find maximum mark and index
+        System.out.println("");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maximum) {
+                maximum = array[i];
+            }
+        }
+        System.out.println("Maximum mark = " + maximum);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == maximum) {
+                System.out.println("Index of max mark = " + i);
+                break;
+            }
+        }
+
+
+    }
+}
